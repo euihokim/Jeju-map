76c912aed25f773301a0527e3395b3985c4a6927
euiho.kim <112460383+euihokim@users.noreply.github.com>
2022-11-27 11:23:29 +0900

    Update 11월 27일

--- index.js
+++ index.js
@@ -8,73 +8,104 @@ function initMap() {
         center: jejuCenter,
     });
 
+    let marker = new google.maps.Marker({
+        position: {
+            lat: 33.435016,
+            lng: 126.495049,
+        },
+        map: map,
+        icon: "./img/airplane.gif",
+    });
+
+    let marker2 = new google.maps.Marker({
+        position: {
+            lat: 33.5066211,
+            lng: 126.49281,
+        },
+        map: map,
+        icon: "./img/dol-hareubang.png",
+    });
+
     const info = [];
 
-    fetch(
-        "https://api.odcloud.kr/api/15096996/v1/uddi:6738a90c-ec96-4245-a187-9528cea62904?page=1&perPage=10&serviceKey=odwzSJ%2BgVLaO6kVbvhVD8PRSNEKy3mxX%2BTnVjjGOo0DKqIWnZjWtKAwzie4OgHNFWFMappAtURVCl8rlp2lboQ%3D%3D"
-    )
-        .then((v) => v.json())
-        .then((data) => {
-            info.push(data.data);
-            let test = {
-                오름명: "제주공항",
-                설명: "제주공항입니다",
-                위도: "33.5066211",
-                경도: "126.49281",
-            };
-            info[0].push(test);
-            // info[0]이 아니라 data.data로도 왜 가능..?
-            let position = info[0];
-            // console.log(data.data);
-            // console.log(info[0]);
-            const infoWindow = new google.maps.InfoWindow();
+    async function test() {
+        let whatInfoWindow = {
+            content: `<p style="text-align: left; font-weight: bold; color: orange; font-size: 16px">welcome everyone~~</p> <br> <p style="text-align: left">`,
+            position: { lat: 33.5066211, lng: 126.49281 },
+            // 고정된 지점에서 너비, 높이 값만큼 움직임 (음수도 가능)
+            pixelOffset: new google.maps.Size(0.5, -30),
+            // 글씨 적힌 창 최대 너비 결정
+            maxWidth: 300,
+        };
 
-            for (let i in position) {
-                let 오름명 = position[i].오름명;
-                let 설명 = position[i].설명;
-                let 위도 = parseFloat(position[i].위도);
-                let 경도 = parseFloat(position[i].경도);
-                // console.log(오름명);
-                // console.log(설명);
-                // console.log(위도);
-                // console.log(경도);
-
-                new google.maps.Circle({
-                    strokeColor: "#66A9C6",
-                    strokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: "#66A9C6",
-                    fillOpacity: 0.35,
-                    map,
-                    center: {
-                        lat: 위도,
-                        lng: 경도,
-                    },
-                    radius: 1000,
-                });
-
-                let marker = new google.maps.Marker({
-                    position: {
-                        lat: 위도,
-                        lng: 경도,
-                    },
-                    map: map,
-                    // icon 바꾸기
-                });
-
-                https: marker.addListener("click", () => {
-                    map.panTo(marker.position);
-                    // 글자 줄바꿈 효과 넣고 싶음
-                    // const explain = `${오름명}: ${설명}`;
-                    const explain = `<p style="text-align: left; font-weight: bold; color: orange; font-size: 16px">${오름명}</p> <br> <p style="text-align: left">${설명}`;
-                    infoWindow.setContent(explain);
-                    infoWindow.open({
-                        anchor: marker,
-                        map,
-                    });
-                });
-            }
+        let markerAnchor = new google.maps.Marker({
+            position: { lat: 33.5066211, lng: 126.49281 },
+            map: map,
         });
-}
 
+        const infoWindow = new google.maps.InfoWindow(whatInfoWindow);
+
+        let infoWindowOpen = {
+            anchor: markerAnchor,
+        };
+
+        infoWindow.open(infoWindowOpen);
+
+        setTimeout(() => {
+            infoWindow.close();
+        }, 5000);
+
+        await fetch(
+            `https://api.odcloud.kr/api/15096996/v1/uddi:6738a90c-ec96-4245-a187-9528cea62904?page=1&perPage=100&serviceKey=odwzSJ%2BgVLaO6kVbvhVD8PRSNEKy3mxX%2BTnVjjGOo0DKqIWnZjWtKAwzie4OgHNFWFMappAtURVCl8rlp2lboQ%3D%3D`
+        )
+            .then((res) => res.json())
+            .then((res) => info.push(res.data));
+
+        for (let i = 0; i < info.flat().length; i++) {
+            let 오름명 = info.flat()[i]["오름명"];
+            let 설명 = info.flat()[i]["설명"];
+            let 위도 = parseFloat(info.flat()[i]["위도"]);
+            let 경도 = parseFloat(info.flat()[i]["경도"]);
+
+            new google.maps.Circle({
+                strokeColor: "#66A9C6",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: "#66A9C6",
+                fillOpacity: 0.35,
+                map,
+                center: {
+                    lat: 위도,
+                    lng: 경도,
+                },
+                radius: 1000,
+            });
+
+            let marker = new google.maps.Marker({
+                position: {
+                    lat: 위도,
+                    lng: 경도,
+                },
+                // icon: "./img/flag.png",
+                map: map, // icon 바꾸기
+            });
+
+            // 다른 infowindow 눌렀을 때 원래 것이 안사라진 이유
+            // const infoWindow = new google.maps.InfoWindow();
+
+            https: marker.addListener("click", () => {
+                map.panTo(marker.position);
+                // 글자let test =  줄바꿈 효과 넣고 싶음
+                // const explain = `${오름명}: ${설명}`;
+                const explain = `<p style="text-align: left; font-weight: bold; color: orange; font-size: 16px">${오름명}</p> <br> <p style="text-align: left">${설명}`;
+                infoWindow.setContent(explain);
+                infoWindow.open({
+                    anchor: marker,
+                    map,
+                });
+            });
+        }
+    }
+    test();
+}
 initMap();
